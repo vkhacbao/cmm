f1d52b6ff6500e4e65f5a1cbc48d6d21f94d699f
vkhacbao2000 <vkhacbao2000@gmail.com>
2023-10-20 13:18:04 +0700

    ok

--- src/main/java/com/example/cmm/CmmApplication.java
+++ src/main/java/com/example/cmm/CmmApplication.java
@@ -2,11 +2,20 @@ package com.example.cmm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CmmApplication {
+public class CmmApplication extends SpringBootServletInitializer
+{
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         application) {
+        return application.sources(CmmApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(CmmApplication.class, args);
     }
 
